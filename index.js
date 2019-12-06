e4f737764dbdc731baa88944a1ecfa8f5d62ad27
Marcus Andersson <marcus.andersson2@klarna.com>
2019-12-06 09:31:01 +0100

    Loads of changes

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "src", "index.html"));
 });
 
-app.listen(8000);
+app.listen(2400);
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
-  // $("body").snowfall({ flakeCount: 100 });
+  function openBox($el) {
+    var height = $(window).height();
+    var width = $(window).width();
+
+    var containerHeight = $(".container").height();
+    var containerWidth = $(".container").height();
+
+    var topPos = (height - containerHeight) / 4;
+    var leftPos = (width - containerWidth) / 4;
+
+    window.setTimeout(function() {
+      $el
+        .css({
+          top: -topPos + "px",
+          left: -leftPos + "px",
+          height: "calc(100% + " + topPos * 2 + "px)",
+          width: "calc(100% + " + leftPos * 2 + "px)"
+        })
+        .addClass("open");
+    }, 0);
+
+    window.setTimeout(function() {
+      $el.removeAttr("style").removeClass("open");
+    }, 2000);
+  }
+
+  $("body").snowfall({ flakeCount: 100 });
 
   const randomDay = Math.floor(Math.random() * 24) + 1;
 
@@ -15,4 +41,16 @@ $(document).ready(function() {
 
     $(".box-" + i).addClass(boxClass);
   }
+
+  $(".box").on("click", function() {
+    var $el = $(this);
+    var classes = $el.attr("class");
+    if (classes.includes("past")) {
+      openBox($el);
+    } else if (classes.includes("present")) {
+      window.setTimeout(openBox, 0, $el);
+      //$(this).addClass("unlock");
+    } else if (classes.includes("future")) {
+    }
+  });
 });
